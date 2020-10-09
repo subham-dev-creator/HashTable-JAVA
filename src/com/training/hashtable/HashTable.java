3004dee6e641ec2e73d8c200c715822eb576649d
subhamstark <subham@sap.com>
2020-10-09 22:51:37 +0530

    UC3 Done

--- src/com/training/hashtable/HashTable.java
+++ src/com/training/hashtable/HashTable.java
@@ -1,12 +1,14 @@
 package com.training.hashtable;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class HashTable<K,V> {
     public int bucketSize ;
     private ArrayList<MapNode<K, V>> bucketArray;
     private int size;
 
+    // Initialize HashTable
     public HashTable()
     {
         bucketArray = new ArrayList<>();
@@ -17,9 +19,13 @@ public class HashTable<K,V> {
             bucketArray.add(null);
     }
 
+    // Return size of HashTable
     public int size() { return size; }
+
+    // Returns HashTable is empty or not
     public boolean isEmpty() { return size() == 0; }
 
+    // returns the bucketIndex for the KEY
     private int getBucketIndex(K key)
     {
         int hashCode = key.hashCode();
@@ -29,6 +35,7 @@ public class HashTable<K,V> {
         return index;
     }
 
+    // Returns the Value for the KEY stored in the HASHTABLE
     public V get(K key) {
 
         int bucketIndex = getBucketIndex(key);
@@ -42,6 +49,7 @@ public class HashTable<K,V> {
         return null;
     }
 
+    // Inserts Key Value pair in the HASHTABLE
     public void put(K key,V value) {
         int bucketIndex = getBucketIndex(key);
         MapNode<K, V> head = bucketArray.get(bucketIndex);
@@ -63,6 +71,38 @@ public class HashTable<K,V> {
         bucketArray.set(bucketIndex, newNode);
     }
 
+    // Remove the key and returns the VALUE for that key
+    public V delete(K key){
+        int bucketIndex = getBucketIndex(key);
+        if(bucketArray.get(bucketIndex)==null)
+            return null;
+        else
+        {
+            size--;
+            MapNode<K,V> node = bucketArray.get(bucketIndex);
+            if(node.getNext()==null || node.getKey()==key)
+            {
+                V val = node.getValue();
+                if(node.getNext()==null)
+                    bucketArray.set(bucketIndex,null);
+                else
+                    bucketArray.set(bucketIndex,node.getNext());
+                return val;
+
+            }
+            else
+            {
+                while(node.getNext()!=null && node.getNext().getKey()!=key){
+                    node = node.getNext();
+                }
+
+                V val = node.getNext().getValue();
+                node.setNext(node.getNext().getNext());
+                return val;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String s = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         String[] array = s.split(" ");
@@ -79,6 +119,14 @@ public class HashTable<K,V> {
         for(String a : array){
             System.out.println(a + " --->> " + hashMap.get(a));
         }
+
+        hashMap.delete("avoidable");
+
+        for(String a : array){
+            System.out.println(a + " --->> " + hashMap.get(a));
+        }
+
+        System.out.println(hashMap.get("avoidable"));
     }
 
 }
